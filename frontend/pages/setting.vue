60237b52fc455f7076fcc2b37880f8a0fdb12a18
soda0408 <soooda0408@gmail.com>
2025-12-20 14:33:16 +0900

    一旦設定画面用のブランチ作った

---
+++ frontend/pages/setting.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="page-container">
+        <header class="main-header">
+            <a href="#" class="hanbargarbar" @click.prevent="goBack">
+                <img src="/images/hanbargarbar-icon.png" alt="メニュー閉じる" />
+            </a>
+            <NuxtLink to="/mypost">>> 自分の投稿を管理する</NuxtLink>
+        </header>
+    </div>
+</template>
+
+<script setup>
+import { useAuthUser } from '../composables/useAuthUser';
+import { useAuthUser } from '../composables/useAuthUser';
+
+const getAuth = () => useAuthUser();
+const router = useRouter();
+
+// 前のページに戻る関数
+const goBack = () => {
+    if (window.history.length > 1) {
+        router.back();
+    } else {
+        router.push('/'); // 履歴がなければトップへ
+    }
+};
+
+defineExpose({ refresh });
+
+const isUserLoggedIn = () => getAuth().isLoggedIn.value;
+const isUserAuthReady = () => getAuth().isAuthReady.value;
+</script>
+
+<style scoped>
+.page-container {
+    background-image: url('/images/background-2.png');
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+
+    width: 100vw;
+    min-height: 100vh;
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+}
+
+.main-header {
+    /*background: #B4EBE6;*/
+    position: sticky;
+    /* スクロールしても上部に残る */
+    top: 0;
+    width: 100%;
+    z-index: 100;
+    margin-bottom: 20px;
+}
+
+.hanbargarbar {
+    width: 80px;
+    height: 80px;
+    top: 160px;
+    left: 60px;
+}
+</style>
